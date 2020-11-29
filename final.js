5f00f20dfe0d76dd5fd21152efcadbf0df796abc
jroxas18 <72133137+jroxas18@users.noreply.github.com>
2020-11-29 08:45:14 -0800

    Update final.js

--- final.js
+++ final.js
@@ -1,21 +1,27 @@
-var location = $('#location');
-var temperature = $('#temperature');
-var description = $('#description');
-
-function commentButton()
+function search()
 {
   var input = $("#locationInput").val();
   
   console.log(input);
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid=3c1c7f97dad91531c9dba623495781dc')
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&units=imperial&appid=3c1c7f97dad91531c9dba623495781dc')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+      var icon = data['weather'][0]['icon'];
+
+      changeData($('#location'), data['name']);
+
+      changeData($('#temperature'), data['main']['temp'] + " °F");
+
+      changeData($('#description'), data['weather'][0]['description']);
+
+      $('#icon').attr('src', 'http://openweathermap.org/img/wn/'+icon+'@4x.png');
+    })
 };
 
 function changeData(tag, newData) {
   
   tag.empty();
 
-  tag.append("newData");
+  tag.append(newData);
 }
